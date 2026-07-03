d6877ef982ce5814e81e3660e25bf6a3da858548
Gabriel LE GALL <gabrielgall@hotmail.fr>
2026-07-03 13:09:35 +0200

    simplified model

--- models/staging/stg_seed__subscription_status.sql
+++ models/staging/stg_seed__subscription_status.sql
@@ -9,7 +9,8 @@ WITH source AS (
         CAST(STATUS AS VARCHAR)                                                                  AS STATUS,
         STATUS = 'active'                                                                        AS IS_ACTIVE,
         LAG(IS_ACTIVE) OVER (PARTITION BY USER_ID ORDER BY DATE_INFO)                            AS PREVIOUS_IS_ACTIVE,
-        MIN(CASE WHEN IS_ACTIVE = TRUE THEN DATE_INFO ELSE NULL END) OVER (PARTITION BY USER_ID) AS FIRST_ACTIVE_DATE_INFO
+        MIN(CASE WHEN IS_ACTIVE = TRUE THEN DATE_INFO ELSE NULL END) OVER (PARTITION BY USER_ID) AS FIRST_ACTIVE_DATE_INFO,
+        CASE WHEN DATE_INFO = FIRST_ACTIVE_DATE_INFO THEN DATE_INFO END                          AS FIRST_ACQUISITION_DATE_INFO,
     FROM source
 )
 
