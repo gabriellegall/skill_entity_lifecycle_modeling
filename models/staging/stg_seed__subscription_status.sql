426443464319204692eea187726b22707983a5c8
Gabriel LE GALL <gabrielgall@hotmail.fr>
2026-07-03 13:36:19 +0200

    renamed col

--- models/staging/stg_seed__subscription_status.sql
+++ models/staging/stg_seed__subscription_status.sql
@@ -1,11 +1,11 @@
 {{ config(materialized='view') }}
 
 SELECT
-    CAST(USER_ID AS INTEGER) AS USER_ID,
-    CAST(DATE_INFO AS DATE) AS DATE_INFO,
-    CAST(STATUS AS VARCHAR) AS STATUS,
-    STATUS = 'active' AS IS_ACTIVE,
-    LAG(STATUS = 'active') OVER (PARTITION BY USER_ID ORDER BY DATE_INFO) AS PREVIOUS_IS_ACTIVE,
+    CAST(USER_ID AS INTEGER)                                      AS USER_ID,
+    CAST(DATE_INFO AS DATE)                                       AS DATE_INFO,
+    CAST(STATUS AS VARCHAR)                                       AS STATUS,
+    STATUS = 'active'                                             AS IS_ACTIVE,
+    LAG(IS_ACTIVE) OVER (PARTITION BY USER_ID ORDER BY DATE_INFO) AS PREVIOUS_IS_ACTIVE,
     CASE
         -- Return TRUE for the first IS_ACTIVE = TRUE at USER_ID level:
         WHEN MIN(CASE WHEN STATUS = 'active' THEN DATE_INFO ELSE NULL END) OVER (PARTITION BY USER_ID) = DATE_INFO THEN TRUE
